b708f15a5586e5f3037bb2d89d95ea75f7397a59
forirodriguez <rodriguezalfonso20@gmail.com>
2025-11-07 11:34:30 -0300

    prisma fix

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
-import { UserRole } from "./lib/generated/prisma/client";
 
 const publicRoutes = ["/", "/login", "/register"];
 
